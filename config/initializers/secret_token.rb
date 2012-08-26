d3b93d02fbcb1c39d4ce4e4ca99b488e413f224d
Kiran Kumara <nikhil@ubuntu.(none)>
2012-08-26 10:28:12 +0530

    my forst app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '6cbb0e95347ad512309c6b200bfac0e8b66baaed66c0d3caa8c950bb89b0c462263960924c9a39a8498738165dd7434858ece30f4b231d5c54623784335bf0ee'
